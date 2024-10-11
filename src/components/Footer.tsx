f9a87f418039df2f1abfabebc694cb97d06bf978
Eddyleon <leon@skhay.com>
2024-10-11 14:29:00 +0300

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,85 @@
+import { Facebook, Github, Gitlab, ScanFace, Languages, Linkedin, Settings } from "lucide-react";
 
 const Footer = () => {
   return (
-    <div className="container">
-      <div>
-        footer
+    <div>
+
+      <div className="bg_svg2">
+        <div className="container flex justify-between gap-4 text-white py-16">
+          {/* first */}
+          <div className="space-y-8">
+            <div className="space-y-4">
+              <div className="flex items-center gap-1">
+                <div>
+                  <ScanFace size={28} />
+                </div>
+                <p className="text-2xl font-medium">
+                  Eddy
+                </p>
+              </div>
+              <p className="max-w-[400px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+                sit amet consectetur adipisicing.
+                Lorem ipsum  consectetur adipisicing.
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+                sit amet consectetur adipisicing.
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <div>
+                <Facebook size={17} />
+              </div>
+              <div>
+                <Linkedin size={17} />
+              </div>
+              <div>
+                <Github size={17} />
+              </div>
+              <div>
+                <Gitlab size={17} />
+              </div>
+            </div>
+          </div>
+          {/* second */}
+          <div className="">
+            <div className="flex gap-6 mb-6">
+              <div>
+                Home
+              </div>
+              <div>
+                Contact
+              </div>
+              <div>
+                Blog
+              </div>
+            </div>
+            <div className="space-y-4">
+              <div className="flex gap-1 items-center">
+                <Languages size={20} />
+                <p>
+                  Language
+                </p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <Settings size={20} />
+                <p>
+                  Settings
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
+      <div className="bg-black border-t border-gray-700">
+        <div className="container text-white py-4">
+          <div>
+            copyright
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,43 +3,45 @@ import { Download } from "lucide-react"
 
 const Hero = () => {
   return (
-    <div className="container py-20">
-      <div className="text-center">
-        <div className="flex justify-center mb-6">
-          <img 
-            src={avatar} 
-            className="w-20 h-20 rounded-full shadow-xl"
-            alt="avatar" 
-          />
-        </div>
-        <div className="text-xl font-medium mb-4">
-          <h3>Salut, Je suis <span className="font-semibold">Eddy</span> 🤟</h3>
-        </div>
-        <div className="font-medium mb-7">
-          <h1 className="text-6xl">
-            Dévellopeur
-          </h1>
-          <h1 className="text-6xl">
-            Frontend
-          </h1>
-        </div>
-        <div className="flex justify-center">
-          <div className="flex items-center gap-4">
-            <div className="space-y-1 text-sm">
-              <div className="bg-green-100 py-1 px-3 rounded-sm border border-green-300">
-                <span className="font-medium">1 year +</span> d'exp
+    <div className="bg_svg">
+      <div className="container py-20">
+        <div className="text-center">
+          <div className="flex justify-center mb-6">
+            <img 
+              src={avatar} 
+              className="w-20 h-20 rounded-full shadow-xl"
+              alt="avatar" 
+            />
+          </div>
+          <div className="text-xl font-medium mb-4">
+            <h3>Salut, Je suis <span className="font-semibold">Eddy</span> 🤟</h3>
+          </div>
+          <div className="font-medium mb-7">
+            <h1 className="text-6xl">
+              Dévellopeur
+            </h1>
+            <h1 className="text-6xl">
+              Frontend
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <div className="flex items-center gap-4">
+              <div className="space-y-1 text-sm">
+                <div className="bg-green-100 py-1 px-3 rounded-sm border border-green-300">
+                  <span className="font-medium">1 year +</span> d'exp
+                </div>
+                <div className="bg-red-100 py-1 px-3 rounded-sm border border-red-300">
+                  <span className="font-medium">+ 27</span> project
+                </div>
               </div>
-              <div className="bg-red-100 py-1 px-3 rounded-sm border border-red-300">
-                <span className="font-medium">+ 27</span> project
+              <div>
+                <button className="bg-gray-900 text-white flex items-center gap-1 py-[20px] px-4 rounded-md">
+                  <Download size={17} />
+                  <span className="text-sm">
+                    Download CV
+                  </span>
+                </button>
               </div>
-            </div>
-            <div>
-              <button className="bg-gray-900 text-white flex items-center gap-1 py-[20px] px-4 rounded-md">
-                <Download size={17} />
-                <span className="text-sm">
-                  Download CV
-                </span>
-              </button>
             </div>
           </div>
         </div>
